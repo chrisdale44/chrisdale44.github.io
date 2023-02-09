d0d38be95922e27a406222e32561e2e605977686
Christopher Dale <chrisdale44@gmail.com>
2023-02-09 19:25:06 +1100

    update animations, fill in content

--- pages/index.js
+++ pages/index.js
@@ -1,48 +1,45 @@
-import dynamic from "next/dynamic";
-import PropTypes from "prop-types";
-const Navigation = dynamic(() => import("../components/Navigation"));
-const Greetings = dynamic(() => import("../containers/Greetings"));
-const Skills = dynamic(() => import("../containers/Skills"));
-const Proficiency = dynamic(() => import("../containers/Proficiency"));
-const Education = dynamic(() => import("../containers/Education"));
-const Experience = dynamic(() => import("../containers/Experience"));
-const Projects = dynamic(() => import("../containers/Projects"));
-const Feedbacks = dynamic(() => import("../containers/Feedbacks"));
+import dynamic from 'next/dynamic'
+import PropTypes from 'prop-types'
+const Navigation = dynamic(() => import('../components/Navigation'))
+const Greetings = dynamic(() => import('../containers/Greetings'))
+const Skills = dynamic(() => import('../containers/Skills'))
+const Education = dynamic(() => import('../containers/Education'))
+const Experience = dynamic(() => import('../containers/Experience'))
+const Projects = dynamic(() => import('../containers/Projects'))
 const GithubProfileCard = dynamic(() =>
-  import("../components/GithubProfileCard")
-);
-import { openSource, showContactUs } from "../portfolio";
-import SEO from "../components/SEO";
-import Contact from "../components/ContactUs.jsx";
+    import('../components/GithubProfileCard')
+)
+import { openSource, showContactUs } from '../portfolio'
+import SEO from '../components/SEO'
+import Contact from '../components/ContactUs.jsx'
 
 export default function Home({ githubProfileData }) {
-  return (
-    <div>
-      <SEO />
-      <Navigation />
-      <Greetings />
-      <Skills />
-      <Proficiency />
-      <Education />
-      <Experience />
-      <Feedbacks />
-      <Projects />
-      {showContactUs ? <Contact /> : null}
-      <GithubProfileCard prof={githubProfileData} />
-    </div>
-  );
+    return (
+        <div>
+            <SEO />
+            <Navigation />
+            <Greetings />
+            <Skills />
+
+            <Experience />
+            <Projects />
+            <Education />
+            {showContactUs ? <Contact /> : null}
+            <GithubProfileCard prof={githubProfileData} />
+        </div>
+    )
 }
 
 Home.prototype = {
-  githubProfileData: PropTypes.object.isRequired,
-};
+    githubProfileData: PropTypes.object.isRequired,
+}
 
 export async function getStaticProps(_) {
-  const githubProfileData = await fetch(
-    `https://api.github.com/users/${openSource.githubUserName}`
-  ).then((res) => res.json());
+    const githubProfileData = await fetch(
+        `https://api.github.com/users/${openSource.githubUserName}`
+    ).then((res) => res.json())
 
-  return {
-    props: { githubProfileData },
-  };
+    return {
+        props: { githubProfileData },
+    }
 }
